fb033d9deaaa96991678ea60ec8f8000797e5072
OmprakashMaurya16 <omprakash.maurya1604@gmail.com>
2026-02-25 18:30:00 +0530

    Authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { Button } from "@/components/ui/button";
+import UserButton from "@/modules/auth/components/user-button";
 
 export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center bg-zinc-100 h-screen">
       <Button>Get Stared</Button>
+      <UserButton />
     </div>
   );
 }
--- auth.ts
+++ auth.ts
@@ -1,45 +1,52 @@
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import { db } from "./lib/db";
-import authConfig from "./auth.config";
-import { getUserById } from "./modules/auth/actions";
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
+import authConfig from "./auth.config";
+import { db } from "./lib/db";
+import { getAccountByUserId, getUserById } from "./modules/auth/actions";
+
+export const { auth, handlers, signIn, signOut } = NextAuth({
   callbacks: {
-    async signIn({ user, account }) {
+    /**
+     * Handle user creation and account linking after a successful sign-in
+     */
+    async signIn({ user, account, profile }) {
       if (!user || !account) return false;
 
+      // Check if the user already exists
       const existingUser = await db.user.findUnique({
         where: { email: user.email! },
       });
 
+      // If user does not exist, create a new one
       if (!existingUser) {
-        const newUser = db.user.create({
+        const newUser = await db.user.create({
           data: {
             email: user.email!,
             name: user.name,
             image: user.image,
 
-            account: {
-              //@ts-expect-error avoid error line
+            accounts: {
+              // @ts-ignore
               create: {
                 type: account.type,
                 provider: account.provider,
                 providerAccountId: account.providerAccountId,
-                refresh_token: account.refresh_token,
-                access_token: account.access_token,
-                expires_at: account.expires_at,
-                token_type: account.token_type,
+                refreshToken: account.refresh_token,
+                accessToken: account.access_token,
+                expiresAt: account.expires_at,
+                tokenType: account.token_type,
                 scope: account.scope,
-                id_token: account.id_token,
-                session_state: account.session_state,
+                idToken: account.id_token,
+                sessionState: account.session_state,
               },
             },
           },
         });
 
-        if (!newUser) return false;
+        if (!newUser) return false; // Return false if user creation fails
       } else {
+        // Link the account if user exists
         const existingAccount = await db.account.findUnique({
           where: {
             provider_providerAccountId: {
@@ -49,21 +56,22 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
           },
         });
 
+        // If the account does not exist, create it
         if (!existingAccount) {
           await db.account.create({
             data: {
-              userId: account.userId,
+              userId: existingUser.id,
               type: account.type,
               provider: account.provider,
               providerAccountId: account.providerAccountId,
-              refresh_token: account.refresh_token,
-              access_token: account.access_token,
-              expires_at: account.expires_at,
-              token_type: account.token_type,
+              refreshToken: account.refresh_token,
+              accessToken: account.access_token,
+              expiresAt: account.expires_at,
+              tokenType: account.token_type,
               scope: account.scope,
-              id_token: account.id_token,
-              //@ts-expect-error ignore  line
-              session_state: account.session_state,
+              idToken: account.id_token,
+              // @ts-ignore
+              sessionState: account.session_state,
             },
           });
         }
@@ -71,12 +79,14 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
 
       return true;
     },
-    async jwt({ token }) {
-      if (!token.sub) return token;
 
+    async jwt({ token, user, account }) {
+      if (!token.sub) return token;
       const existingUser = await getUserById(token.sub);
 
       if (!existingUser) return token;
+
+      const exisitingAccount = await getAccountByUserId(existingUser.id);
 
       token.name = existingUser.name;
       token.email = existingUser.email;
@@ -84,7 +94,9 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
 
       return token;
     },
+
     async session({ session, token }) {
+      // Attach the user ID from the token to the session
       if (token.sub && session.user) {
         session.user.id = token.sub;
       }
@@ -96,7 +108,9 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       return session;
     },
   },
+
   secret: process.env.AUTH_SECRET,
   adapter: PrismaAdapter(db),
+  session: { strategy: "jwt" },
   ...authConfig,
 });
