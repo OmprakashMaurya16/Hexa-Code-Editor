20f99f5b33238086adbd10a06573b9f97f166536
OmprakashMaurya16 <omprakash.maurya1604@gmail.com>
2026-02-25 11:31:57 +0530

    Initial Nextjs and shadcn setup

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col justify-center items-center bg-zinc-100 h-screen">
+      <Button>Get Stared</Button>
+    </div>
+  );
+}
